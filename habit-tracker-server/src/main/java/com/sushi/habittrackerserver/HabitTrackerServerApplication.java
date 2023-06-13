61ecba1ed0335a5874983b6d21cd05905ac89b10
Kuan-Wei-Kuo <k.kuanwei@gmail.com>
2023-06-13 22:27:03 +0800

    init habit tracker server project

---
+++ habit-tracker-server/src/main/java/com/sushi/habittrackerserver/HabitTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.sushi.habittrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitTrackerServerApplication.class, args);
+	}
+
+}
---
+++ habit-tracker-server/src/test/java/com/sushi/habittrackerserver/HabitTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushi.habittrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
